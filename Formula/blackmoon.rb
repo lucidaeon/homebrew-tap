70dc8a1f4eaf381b4edb39c2d143c53a9f491d5f
lucidaeon <lucidaeon@users.noreply.github.com>
2026-07-08 13:12:11 -0700

    Remove redundant version stanza (scanned from URL) to fix brew audit

--- Formula/blackmoon.rb
+++ Formula/blackmoon.rb
@@ -2,7 +2,6 @@ class Blackmoon < Formula
   desc "Reads any chart format, merges, writes any format"
   homepage "https://github.com/lucidaeon/mediumcoeli"
   url "https://github.com/lucidaeon/mediumcoeli/archive/refs/tags/blackmoon/0.4.0.tar.gz"
-  version "0.4.0"
   sha256 "27bdd7f94e6e4bb3ae4faedbd17756750f345145af891c2440d62e7ae3e941ed"
   license "CC0-1.0"
   head "https://github.com/lucidaeon/mediumcoeli.git", branch: "main"
