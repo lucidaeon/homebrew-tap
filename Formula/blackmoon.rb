d8e4aa88095b0e6044b4a227c69afe6f31d8ae61
lucidaeon <lucidaeon@users.noreply.github.com>
2026-07-08 20:16:46 -0700

    blackmoon 0.4.2
    Closes #1.
    
    Signed-off-by: lucidaeon <lucidaeon@users.noreply.github.com>

--- Formula/blackmoon.rb
+++ Formula/blackmoon.rb
@@ -1,8 +1,8 @@
 class Blackmoon < Formula
   desc "Reads any chart format, merges, writes any format"
   homepage "https://github.com/lucidaeon/mediumcoeli"
-  url "https://github.com/lucidaeon/mediumcoeli/archive/refs/tags/blackmoon/0.4.0.tar.gz"
-  sha256 "27bdd7f94e6e4bb3ae4faedbd17756750f345145af891c2440d62e7ae3e941ed"
+  url "https://github.com/lucidaeon/mediumcoeli/archive/refs/tags/blackmoon/0.4.2.tar.gz"
+  sha256 "bbc5b9aba932a3256dd0987aee46a5239820da44ef22e8b604adb4abf61f7cd1"
   license "CC0-1.0"
   head "https://github.com/lucidaeon/mediumcoeli.git", branch: "main"
 
