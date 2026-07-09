df764088b29b2a49023f608a706c5f129e148036
lucidaeon <lucidaeon@users.noreply.github.com>
2026-07-08 20:27:32 -0700

    blackmoon: add 0.4.2 bottle.

--- Formula/blackmoon.rb
+++ Formula/blackmoon.rb
@@ -6,6 +6,11 @@ class Blackmoon < Formula
   license "CC0-1.0"
   head "https://github.com/lucidaeon/mediumcoeli.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/lucidaeon/homebrew-tap/releases/download/blackmoon-0.4.2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "ebbb57f8bd273ed37a4a2dc036f98333f27472fe0188c86f54a11882ffaefaea"
+  end
+
   depends_on "rust" => :build
 
   def install
