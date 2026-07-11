2ae18046f00501633ed9b683aeca54764d151447
lucidaeon <lucidaeon@users.noreply.github.com>
2026-07-11 06:32:05 +0000

    starcat 0.11.0

--- Formula/starcat.rb
+++ Formula/starcat.rb
@@ -1,8 +1,8 @@
 class Starcat < Formula
   desc "Ephemeris computation and table presentation CLI"
   homepage "https://github.com/lucidaeon/mediumcoeli"
-  url "https://github.com/lucidaeon/mediumcoeli/archive/refs/tags/starcat/0.10.0.tar.gz"
-  sha256 "68517f8caf4fcab61aba4272e5e38930f61d7bed87a89d00d9ac6797afb4edfe"
+  url "https://github.com/lucidaeon/mediumcoeli/archive/refs/tags/starcat/0.11.0.tar.gz"
+  sha256 "0f7a36e68315d67e40329b9a38e636511a6f1218381c97fdcfe233fa1dcb8eee"
   license "CC0-1.0"
   head "https://github.com/lucidaeon/mediumcoeli.git", branch: "main"
 
