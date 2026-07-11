5a7f5a0234aa1a51dc241c0a0e3e4e82b8450186
lucidaeon <lucidaeon@users.noreply.github.com>
2026-07-10 17:22:31 -0700

    Revert "Bump starcat to 0.11.0 (#6)"
    This reverts commit fc3d9fd10f9ff48f888c69ceb46718fbd4ac0926.

--- Formula/starcat.rb
+++ Formula/starcat.rb
@@ -1,14 +1,14 @@
 class Starcat < Formula
   desc "Ephemeris computation and table presentation CLI"
   homepage "https://github.com/lucidaeon/mediumcoeli"
-  url "https://github.com/lucidaeon/mediumcoeli/archive/refs/tags/citest/starcat/0.11.0.tar.gz"
-  sha256 "f71d081653c2c3b1eb549929792bbf2146c302758b8922887d7681d6dec1efde"
+  url "https://github.com/lucidaeon/mediumcoeli/archive/refs/tags/starcat/0.10.0.tar.gz"
+  sha256 "68517f8caf4fcab61aba4272e5e38930f61d7bed87a89d00d9ac6797afb4edfe"
   license "CC0-1.0"
   head "https://github.com/lucidaeon/mediumcoeli.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/lucidaeon/mediumcoeli/releases/download/citest/starcat/0.11.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "66e7fd121f9625f08d492366fe9337972e71d17f91b50ca2f63addcb4f717f0f"
+    root_url "https://github.com/lucidaeon/mediumcoeli/releases/download/starcat/0.10.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "18e64f4d184b19e9b21f8a02ec921c7616771940061a78629bb946c8835212e2"
   end
 
   depends_on "rust" => :build
