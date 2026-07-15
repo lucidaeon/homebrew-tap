946c518cea7ac79f332221dec0aaeb26ca136073
lucidaeon <3385549+lucidaeon@users.noreply.github.com>
2026-07-15 15:53:00 -0700

    Bump blackmoon to 0.6.0 (#13)
    Co-authored-by: lucidaeon <lucidaeon@users.noreply.github.com>

--- Formula/blackmoon.rb
+++ Formula/blackmoon.rb
@@ -1,14 +1,14 @@
 class Blackmoon < Formula
   desc "Reads any chart format, merges, writes any format"
   homepage "https://github.com/lucidaeon/mediumcoeli"
-  url "https://github.com/lucidaeon/mediumcoeli/archive/refs/tags/blackmoon/0.5.0.tar.gz"
-  sha256 "fbdcc904da11f3236c2548b76c52d1764d4ce13dcd8c353ef2a7c968d9e419b4"
+  url "https://github.com/lucidaeon/mediumcoeli/archive/refs/tags/blackmoon/0.6.0.tar.gz"
+  sha256 "7b46343cb9372a7e2baf4073332e379650cef564d208ed8b00a4ec175722b499"
   license "CC0-1.0"
   head "https://github.com/lucidaeon/mediumcoeli.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/lucidaeon/mediumcoeli/releases/download/blackmoon/0.5.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "3be094eac77621f69211a838b59d7f4be6ec8718c95ae327a5fd974a51ba84d2"
+    root_url "https://github.com/lucidaeon/mediumcoeli/releases/download/blackmoon/0.6.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "bc7267e562a2ff02eb9d6085e50ba313ca0130a55f49e0328be7607e06f59037"
   end
 
   depends_on "rust" => :build
