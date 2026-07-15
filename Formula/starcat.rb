64857fc4253f7a964d4cb1d8534785ebca6e1d6f
lucidaeon <3385549+lucidaeon@users.noreply.github.com>
2026-07-15 15:52:55 -0700

    Bump starcat to 0.13.0 (#12)
    Co-authored-by: lucidaeon <lucidaeon@users.noreply.github.com>

--- Formula/starcat.rb
+++ Formula/starcat.rb
@@ -1,14 +1,14 @@
 class Starcat < Formula
   desc "Ephemeris computation and table presentation CLI"
   homepage "https://github.com/lucidaeon/mediumcoeli"
-  url "https://github.com/lucidaeon/mediumcoeli/archive/refs/tags/starcat/0.12.0.tar.gz"
-  sha256 "1f1f0cffd3db93b01722d4021ff56611641fbf7e503ecc82e464c5c69630ffd6"
+  url "https://github.com/lucidaeon/mediumcoeli/archive/refs/tags/starcat/0.13.0.tar.gz"
+  sha256 "d587611f1d43d73f9845780a65064d5262c92688f780b284959c657e15f816b8"
   license "CC0-1.0"
   head "https://github.com/lucidaeon/mediumcoeli.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/lucidaeon/mediumcoeli/releases/download/starcat/0.12.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "7dc5ca881de882cb30e78c9fe571c60da158d604f0c768d8927d81cc3b42fe0e"
+    root_url "https://github.com/lucidaeon/mediumcoeli/releases/download/starcat/0.13.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "0377bdbba12e0a094fae17eab88f250870c172d19d4bfe65ab4614fed6ae2cae"
   end
 
   depends_on "rust" => :build
